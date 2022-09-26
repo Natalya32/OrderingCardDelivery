be3eba0f3f560a405c8ea341df03d434df78dc35
Natalya32 <tigra-pochta@mail.ru>
2022-09-26 22:43:22 +0300

    Add generationDate and date check pushUp

--- src/test/java/ru/netology/rest/OrderDeliveryTest.java
+++ src/test/java/ru/netology/rest/OrderDeliveryTest.java
@@ -1,35 +1,54 @@
 package ru.netology.rest;
 
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class OrderDeliveryTest {
-    @Test
-    void submitFormPass () {
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @BeforeEach
+    void openBrowser() {
         open("http://localhost:9999");
+    }
+
+    @Test
+    void submitFormPass() {
+        String date = generateDate(7);
         $("[placeholder=\"Город\"]").setValue("Брянск");
-        $("[placeholder=\"Дата встречи\"]").setValue("24.10.2022");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder=\"Дата встречи\"]").setValue(date);
         $("[name=\"name\"]").setValue("Петров Петя");
         $("[name=\"phone\"]").setValue("+79001112233");
         $x("//input[@name=\"agreement\"]/..").click();
         $x("//span[contains(text(),\"Забронировать\")]").click();
-        $x("//div[contains(text(),\"Успешно!\")]").should(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + date), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
-        @Test
-        void submitFormFail () {
-            open("http://localhost:9999");
-            $("[placeholder=\"Город\"]").setValue("Брянск");
-            $("[placeholder=\"Дата встречи\"]").setValue("24.10.2022");
-            $("[name=\"name\"]").setValue("Петров Пётр");
-            $("[name=\"phone\"]").setValue("+79011111111");
-            $x("//input[@name=\"agreement\"]/..").click();
-            $x("//span[contains(text(),\"Забронировать\")]").click();
-            $x("//div[contains(text(),\"Успешно!\")]").should(visible, Duration.ofSeconds(15));
-        }
+    @Test
+    void submitFormFail() {
+        String date = generateDate(7);
+        $("[placeholder=\"Город\"]").setValue("Брянск");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder=\"Дата встречи\"]").setValue(date);
+        $("[name=\"name\"]").setValue("Петров Пётр");
+        $("[name=\"phone\"]").setValue("+79011111111");
+        $x("//input[@name=\"agreement\"]/..").click();
+        $x("//span[contains(text(),\"Забронировать\")]").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + date), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+    }
 }
